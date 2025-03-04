82e14d25a76559af22a3f2e4a6e8e354a7442f59
jjy <jjyruby@gmail.com>
2025-03-04 15:45:51 +0800

    Add CI

--- src/graph_source/rpc.rs
+++ src/graph_source/rpc.rs
@@ -42,7 +42,7 @@ impl RPCGraphSource {
 #[allow(clippy::manual_async_fn)]
 impl GraphSource for RPCGraphSource {
     fn node_info(&self) -> impl Future<Output = Result<NodeInfoResult>> {
-        async { self.fiber_client.node_info().await.map_err(Into::into) }
+        async { self.fiber_client.node_info().await }
     }
 
     fn graph_nodes(&self) -> impl Future<Output = Result<Vec<NodeInfo>>> {
@@ -55,7 +55,6 @@ impl GraphSource for RPCGraphSource {
                 })
                 .await
                 .map(|r| r.nodes)
-                .map_err(Into::into)
         }
     }
 
@@ -69,7 +68,6 @@ impl GraphSource for RPCGraphSource {
                 })
                 .await
                 .map(|r| r.channels)
-                .map_err(Into::into)
         }
     }
 
@@ -82,7 +80,6 @@ impl GraphSource for RPCGraphSource {
                 })
                 .await
                 .map(|r| r.channels)
-                .map_err(Into::into)
         }
     }
 
@@ -94,7 +91,6 @@ impl GraphSource for RPCGraphSource {
                     save: Some(true),
                 })
                 .await
-                .map_err(Into::into)
         }
     }
 
@@ -104,7 +100,6 @@ impl GraphSource for RPCGraphSource {
                 .open_channel(params)
                 .await
                 .map(|r| r.temporary_channel_id)
-                .map_err(Into::into)
         }
     }
 
